9b4d09715c6def091cefe07298ca0ab09e0c8999
César Márquez <cmrodriguez17@esei.uvigo.es>
2021-04-24 21:15:05 +0200

    SPFFilter siempre comprueba e IPs vacías por defecto

--- spamfilter/filtering/filters/SPFFilter.py
+++ spamfilter/filtering/filters/SPFFilter.py
@@ -16,16 +16,15 @@ class SPFFilter(DBFilter):
         :return: True, if the sender's IP does not belong to the sender's domain (detected as spam). False, if it does.
         """
 
-        # Get sender domain and check if we have data for it. If we don't then look up the IP ranges and return False.
-        # If we do then check the IP.
+        # Get sender domain and check if we have data for it. If we don't then look up the IP ranges.
+        # Check if the IP belongs to the domain.
         domain = envelope.get_sender_domain()
         if domain not in self.data:
             self.data[domain] = envelope.get_all_sender_ip_ranges()
-            return False
-        else:
-            sender_ip = envelope.peer[0]
-            for ip_range in self.data[domain]:
-                if ipaddress.ip_address(sender_ip) in ipaddress.ip_network(ip_range):
-                    return False
-            logging.info(f"Sender IP '{sender_ip}' does not belong to the sender domain '{domain}'")
-            return True
+
+        sender_ip = envelope.peer[0]
+        for ip_range in self.data[domain]:
+            if ipaddress.ip_address(sender_ip) in ipaddress.ip_network(ip_range):
+                return False
+        logging.info(f"Sender IP '{sender_ip}' does not belong to the sender domain '{domain}'")
+        return True
